24472dca26d0b0736524cf0f07c061fed678e24d
Carson McMillan <carsonm@gmail.com>
2013-08-30 10:10:28 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LSHire::Application.config.secret_key_base = '56ff3b4151402451fed997e217792c8ee508bac691e8ffbb7afce7242ff401b5f80a5aae8b3c4342f107b31615c0720363bc6fa6e3346f3295dd86ee503d85ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LSHire::Application.config.session_store :cookie_store, key: '_LSHire_session'
